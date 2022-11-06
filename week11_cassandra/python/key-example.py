04e7321381a523234e78b4b1de6ff93d71008a11
Ubuntu <ubuntu@ip-172-31-35-248.us-east-2.compute.internal>
2022-11-06 21:08:15 +0000

    updates

--- week11_cassandra/python/key-example.py
+++ week11_cassandra/python/key-example.py
@@ -2,6 +2,9 @@
 
 import logging
 import uuid
+import random
+from datetime import date
+
 
 log = logging.getLogger()
 log.setLevel('INFO')
@@ -12,18 +15,32 @@ log.addHandler(handler)
 from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
+from cassandra.util import Date
 
 KEYSPACE = "testkeyspace"
 
+# for testing, creates a payment tuple with random values
+names = ["dave","mary","ashwin","john","archana","akira"]
+def make_payment():
+    name = names[random.randrange(0,len(names)-1)]
+    # dt = Date(2022,11,random.randrange(1,7))
+    dt = date.today()
+    uuid_ = str(uuid.uuid4())
+    amt = round(random.uniform(0, 1) * 100,2)
+    return (name,dt,uuid_,amt)
+
 def main():
+    # connect to the cluster
     cluster = Cluster(['127.0.0.1'])
     session = cluster.connect()
 
+    # if the keyspace already exists then drop it
     rows = session.execute("SELECT keyspace_name FROM system_schema.keyspaces")
     if KEYSPACE in [row[0] for row in rows]:
         log.info("dropping existing keyspace...")
         session.execute("DROP KEYSPACE " + KEYSPACE)
 
+    # create the keyspace
     log.info("creating keyspace...")
     session.execute("""
         CREATE KEYSPACE %s
@@ -33,14 +50,15 @@ def main():
     log.info("setting keyspace...")
     session.set_keyspace(KEYSPACE)
 
+    # create the table
     log.info("creating table...")
     session.execute("""
-        CREATE TABLE mytable (
-            country text,
-            url text,
+        CREATE TABLE payment_table (
+            name text,
+            dt timestamp,
             uuid text,
-            col2 text,
-            PRIMARY KEY (country, url,uuid)
+            amt decimal,
+            PRIMARY KEY (name,dt,uuid)
         )
         """)
 
@@ -49,21 +67,20 @@ def main():
     #     VALUES (%(key)s, %(a)s, %(b)s)
     #     """, consistency_level=ConsistencyLevel.ONE)
 
+    # prepared statement query to insert values
     prepared = session.prepare("""
-        INSERT INTO mytable (country, url, uuid,col2)
+        INSERT INTO payment_table (name, dt, uuid,amt)
         VALUES (?, ?, ?, ?)
         """)
 
-    countries = ["ie","us","fr","uk"]
-    urls = ["u1","u2","u3","u4"]
-
-    for i in range(1000):
+    # insert rows
+    for i in range(10):
         log.info("inserting row %d" % i)
-        # session.execute(query, dict(key="key%d" % i, a='a', b='b'))
-        session.execute(prepared.bind((countries[i % len(countries)], urls[i % len(urls)], str(uuid.uuid4()), 'b')))
+        pmt = make_payment()
+        session.execute(prepared.bind(pmt))
 
-    # future = session.execute_async("SELECT * FROM mytable")
-    future = session.execute_async("SELECT country,url,count(*) FROM mytable group by country,url")
+    # query results, aggregate (sum) amt by name and date (dt)
+    future = session.execute_async("SELECT name, dt, sum(amt) FROM payment_table group by name, dt")
     log.info("key\tcol1\tcol2")
     log.info("---\t----\t----")
 
@@ -72,10 +89,13 @@ def main():
     except Exception:
         log.exeception()
 
+    # iterate the result rows
     for row in rows:
+        # map cols in row to a string type
         row = map(lambda c: str(c),row)
         log.info('\t'.join(row))
 
+    # delete the keyspace when done
     session.execute("DROP KEYSPACE " + KEYSPACE)
 
 if __name__ == "__main__":
